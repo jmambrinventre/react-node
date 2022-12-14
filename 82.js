ddc2c35609fcb4536442a2a2df59f554a16fbc2c
jmambrinventre <jmambrinventre@gmail.com>
2022-12-14 12:20:43 -0300

    100/113

---
+++ 82.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card <= 6) 
+    count++;
+  else if (card >="10")
+    count--;
+  return count + (count > 0 ? ' Bet' : ' Hold');
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
---
+++ 93.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "H",
+      "title": "O",
+      "release_year": 1,
+      "formats": [ 
+        "L",
+        "A",
+        "A"
+      ]
+    }
+  ];
